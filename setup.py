709f73a66bd2a647a0df15b5bfb550a21545a880
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-02-24 15:41:38 +0100

    fix description content type

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=requirements,
     license="BSD license",
     long_description=readme + "\n\n" + history,
+    long_description_content_type="text/x-rst",
     include_package_data=True,
     keywords="python_dev_tools",
     name="python_dev_tools",
