4107ae116050b26ec9a69c67fe508eb7c4a176be
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-02-25 08:39:28 +0100

    add deliver.sh

---
+++ deliver.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source venv/bin/activate
+
+echo -n "Do you want to add something in HISTORY.rst? (y/n) [n]:"
+read BOOL
+if [ "$BOOL" == "y" ]
+then
+    exit 1
+fi
+echo ""
+
+echo "Running test"
+python setup.py develop
+make test
+echo -n "Is it OK? (y/n) [y]:"
+read BOOL
+if [ "$BOOL" == "n" ]
+then
+    exit 1
+fi
+echo ""
+
+echo "Delivering"
+make bump
+make release
+git push
+git push --tags
+echo "Edit the release on GitHub (https://github.com/vpoulailleau/python-dev-tools/releases)"
+echo "Done"
+
