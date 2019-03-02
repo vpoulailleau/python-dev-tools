a6fffc246546bd00837e2802fbb5bca7801880d9
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-03-02 10:55:36 +0100

    update tests

--- tests/test_python_dev_tools.py
+++ tests/test_python_dev_tools.py
@@ -17,7 +17,7 @@ def test_long_line(tmpdir):
     p = tmpdir.join("foo.py")
     p.write('"""Docstring."""\n\n"' + 78 * "#" + '"\n')
     result = lint(p)
-    assert result == {
+    assert result == [
         LinterMessage(
             tool="pycodestyle",
             message_id="E501",
@@ -27,24 +27,24 @@ def test_long_line(tmpdir):
             message="line too long (80 > 79 characters)",
             extramessage="",
         )
-    }
+    ]
 
 
 def test_duplicate_key(tmpdir):
     p = tmpdir.join("foo.py")
     p.write('"""Docstring."""\n\na = {1: 5, 1: 6}\n')
     result = lint(p)
-    assert result == {
+    assert result == [
         LinterMessage(
             tool="pyflakes",
             message_id="W999",
             filename=str(p),
             lineno=3,
-            charno=0,
+            charno=1,
             message="dictionary key 1 repeated with different values",
             extramessage="",
         )
-    }
+    ]
 
 
 # TODO test mccabe
@@ -54,17 +54,17 @@ def test_no_docstring(tmpdir):
     p = tmpdir.join("foo.py")
     p.write("a = 3\n")
     result = lint(p)
-    assert result == {
+    assert result == [
         LinterMessage(
             tool="pydocstyle",
             message_id="D100",
             filename=str(p),
             lineno=1,
-            charno=0,
+            charno=1,
             message="Missing docstring in public module",
             extramessage="at module level",
         )
-    }
+    ]
 
 
 def test_lint_myself():
