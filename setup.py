874008c851d985af8f84373973ecc4d48b602080
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-06-15 18:21:19 +0200

    Bump version: 2019.06.13 → 2019.06.15

--- setup.py
+++ setup.py
@@ -76,6 +76,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vpoulailleau/python-dev-tools",
-    version="2019.06.13",
+    version="2019.06.15",
     zip_safe=False,
 )
