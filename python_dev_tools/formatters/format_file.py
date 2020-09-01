70c78794ac0b41915b69de5bd531ee8eda330b51
Vincent Poulailleau <vpoulailleau@gmail.com>
2020-09-01 17:26:16 +0200

    add type annotations and format docstring

--- python_dev_tools/formatters/format_file.py
+++ python_dev_tools/formatters/format_file.py
@@ -2,16 +2,21 @@
 
 from python_dev_tools.formatters.autoflake import AutoflakeFormatter
 from python_dev_tools.formatters.black import BlackFormatter
+from python_dev_tools.formatters.common import Formatter
 from python_dev_tools.formatters.pyupgrade import PyupgradeFormatter
 
-formatters = [
+formatters: list[Formatter] = [
     AutoflakeFormatter,
     PyupgradeFormatter,
     BlackFormatter,  # BlackFormatter should be the last one
 ]
 
 
-def format_file(filepath):
-    """Format the file with known formatters."""
+def format_file(filepath: str) -> None:
+    """Format the file with known formatters.
+
+    Args:
+        filepath (str): path of the file to format
+    """
     for formatter in formatters:
         formatter.format_file(filepath)
