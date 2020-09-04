cd1bcf754ff367ca3d5a06c299ad2ca1553b4589
Vincent Poulailleau <vpoulailleau@gmail.com>
2020-09-04 07:45:37 +0200

    lint

--- whatalinter_vscode.py
+++ whatalinter_vscode.py
@@ -1,15 +1,19 @@
-import subprocess
-import sys
+"""
+Wrapper for whatalinter inside Visual Studio Code.
 
-# hack for vs code!!!!
+VS Code wants to import ``whatalinter``, not to execute it.
+When ``whatalinter_vscode`` is imported, ``whatalinter`` is executed.
+"""
+import subprocess  # noqa: S404
+import sys
 
 new_args = ["whatalinter"]
 for arg in sys.argv[1:]:
     if "--format" in arg:
         new_args.append("--format")
-        new_args.append('"{}"'.format(arg.split("=")[-1]))
+        new_args.append('"{0}"'.format(arg.split("=")[-1]))
     else:
         new_args.append(arg)
-out = subprocess.run(" ".join(new_args), capture_output=True, shell=True)
+out = subprocess.run(" ".join(new_args), capture_output=True, shell=True)  # noqa: S602
 print(out.stdout.decode("utf-8"))
 print(out.stderr.decode("utf-8"), file=sys.stderr)
