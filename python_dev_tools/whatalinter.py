214fc7f0f74de89d792b1dc1b46aca673cab0743
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-05-26 12:32:08 +0200

    refactoring

--- python_dev_tools/whatalinter.py
+++ python_dev_tools/whatalinter.py
@@ -1,23 +1,43 @@
+import ast
+from typing import TYPE_CHECKING, Final, Type, List
+
 import pkg_resources
-from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from flake8.options.manager import OptionManager
+
+
+MAX_LINE_LENGTH: Final = 88
+MAX_COMPLEXITY: Final = 10
+ERROR_CODE: Final = "WAL"
 
 
 class WhatALinter:
     name = "whatalinter"
     version = pkg_resources.get_distribution("python_dev_tools").version
 
-    def __init__(self, tree):
-        pass
+    def __init__(self: "WhatALinter", tree: ast.AST) -> None:
+        """Empty method, needed by flake8.
 
-    def run(self):
+        Args:
+            tree (AST): unused
+        """
+
+    def run(self: "WhatALinter") -> List:
+        """Empty method, needed by flake8.
+
+        Returns:
+            Empty list
+        """
         return []
 
     @classmethod
-    def add_options(cls, parser: "OptionManager"):
-        ERROR_CODE = "WAL"
+    def add_options(cls: Type["WhatALinter"], parser: "OptionManager") -> None:
+        """Add options to CLI parser.
+
+        Args:
+            parser (OptionManager): CLI parser
+        """
         parser.extend_default_select([ERROR_CODE])
         parser.extend_default_ignore(
             [
@@ -25,9 +45,10 @@ class WhatALinter:
                 "WPS305",  # avoid f-strings
                 "WPS306",  # required explicit subclassing of object
                 "WPS421",  # Found wrong function call, useful in scripts
-                # "WPS602",  # avoid @staticmethod (can be subclassed…)
-            ]
+            ],
         )
         parser.parser.set_defaults(
-            max_line_length=88, max_complexity=10, inline_quotes='"'
+            max_line_length=MAX_LINE_LENGTH,
+            max_complexity=MAX_COMPLEXITY,
+            inline_quotes='"',
         )
